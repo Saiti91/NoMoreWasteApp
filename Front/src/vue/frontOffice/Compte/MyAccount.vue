c940e55e0e27e0e493515dcf6bcc3f735792d40b
ambre <ambre.laurent21@gmail.com>
2024-08-22 17:10:15 +0200

    Update myAccount for modify information

--- Front/src/vue/frontOffice/Compte/MyAccount.vue
+++ Front/src/vue/frontOffice/Compte/MyAccount.vue
@@ -8,19 +8,106 @@ import Swal from "sweetalert2";
 import { useI18n } from 'vue-i18n';
 
 const user = ref(null);
+const originalUser = ref(null);  // Pour stocker les valeurs originales
+const newPassword = ref('');
+const confirmPassword = ref('');
+const showNewPassword = ref(false);
+const showCurrentPassword = ref(false);
+const showConfirmPassword = ref(false);
 const t = useI18n().t;
 const route = useRoute();
 const router = useRouter();
 
+// Formater la date de naissance pour l'input
+const formatDateForInput = (date) => {
+  if (!date) return '';
+  const parsedDate = new Date(date);
+  const year = parsedDate.getFullYear();
+  const month = String(parsedDate.getMonth() + 1).padStart(2, '0');
+  const day = String(parsedDate.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
+};
+
+// Récupère infos de l'utilisateur
 const fetchUserDetails = async () => {
   try {
     const response = await axios.get(`/users/${route.params.id}`);
     user.value = response.data;
+    originalUser.value = { ...response.data }; // Stocker les valeurs originales
+    user.value.Birthdate = formatDateForInput(user.value.Birthdate);
   } catch (error) {
     console.error('Error fetching user details:', error);
   }
 };
 
+//Vérification des champs
+const validateFields = () => {
+  // Vérifier si tous les champs qui avaient une valeur initiale en ont toujours une
+  for (const key in originalUser.value) {
+    if (originalUser.value[key] && !user.value[key]) {
+      Swal.fire({
+        icon: 'error',
+        title: t('errorUpdateUserTitle'),
+        text: t('errorUpdateUserLeftFields'),
+      });
+      return false;
+    }
+  }
+
+  // Vérifier la correspondance des mots de passe
+  if (newPassword.value && newPassword.value !== confirmPassword.value) {
+    Swal.fire({
+      icon: 'error',
+      title: t('errorUpdateUserTitle'),
+      text: t('pwd-dont-match'),
+    });
+    return false;
+  }
+
+  return true;
+};
+
+// Mettre à jour les infos utilisateur
+const updateUserDetails = async () => {
+  if (!validateFields()) return;
+
+  const updateData = {
+    first_name: user.value.Firstname,
+    last_name: user.value.Name,
+    email: user.value.Email,
+    telephone: user.value.Phone,
+    birthdate: user.value.Birthdate,
+    address: {
+      street: user.value.Street,
+      city: user.value.City,
+      state: user.value.State,
+      postal_code: user.value.Postal_Code,
+      country: user.value.Country,
+    },
+  };
+
+  if (newPassword.value) {
+    updateData.password = newPassword.value;
+  }
+
+  try {
+    await axios.patch(`/users/${route.params.id}`, updateData);
+    Swal.fire({
+      icon: 'success',
+      title: t('successUpdateUserTitle'),
+      text: t('successUpdateUserTxt'),
+    });
+  } catch (error) {
+    console.error('Error updating user details:', error);
+    Swal.fire({
+      icon: 'error',
+      title: t('errorUpdateUserTitle'),
+      text: t('errorUpdateUserTxt'),
+    });
+  }
+};
+
+// Supprimer son compte
 const deleteUser = async () => {
   Swal.fire({
     title: t('popupDelUserFO'),
@@ -30,7 +117,7 @@ const deleteUser = async () => {
     confirmButtonColor: '#3085d6',
     cancelButtonColor: '#d33',
     confirmButtonText: t('yesDel'),
-    cancelButtonText: t('cancel')
+    cancelButtonText: t('cancel'),
   }).then(async (result) => {
     if (result.isConfirmed) {
       try {
@@ -50,7 +137,6 @@ const deleteUser = async () => {
 };
 
 const validateSuppression = () => {
-  // Afficher une pop-up de confirmation de suppression
   Swal.fire({
     icon: 'success',
     title: t('deleted'),
@@ -58,16 +144,11 @@ const validateSuppression = () => {
   });
 };
 
-const formatDate = (date) => {
-  if (!date) return t('noInfo');
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  return new Date(date).toLocaleDateString('fr-FR', options);
-};
-
 onMounted(() => {
   fetchUserDetails();
 });
 </script>
+
 
 <template>
   <Header />
@@ -81,22 +162,65 @@ onMounted(() => {
           <button @click="deleteUser" class="ui red button">{{ t('delete') }}</button>
         </div>
         <div v-if="user" class="user-details">
-          <p><strong>{{ t('last-name') }} :</strong> {{ user.Name || t('noInfo') }}</p>
-          <p><strong>{{ t('first-name') }} :</strong> {{ user.Firstname || t('noInfo') }}</p>
-          <p><strong>{{ t('email') }} :</strong> {{ user.Email }}</p>
-          <p><strong>{{ t('phone') }} :</strong> {{ user.Phone || t('noInfo') }}</p>
-          <p><strong>{{ t('role') }} :</strong> {{ user.Role }}</p>
-          <p><strong>{{ t('birthdate') }} :</strong> {{ formatDate(user.Birthdate) }}</p>
+          <p><strong>{{ t('last-name') }} :</strong>
+            <input v-model="user.Name" type="text" class="ui input styled-input" required />
+          </p>
+          <p><strong>{{ t('first-name') }} :</strong>
+            <input v-model="user.Firstname" type="text" class="ui input styled-input" required />
+          </p>
+          <p><strong>{{ t('email') }} :</strong>
+            <input v-model="user.Email" type="email" class="ui input styled-input" required />
+          </p>
+          <p><strong>{{ t('phone') }} :</strong>
+            <input v-model="user.Phone" type="tel" class="ui input styled-input" />
+          </p>
+          <p><strong>{{ t('birthdate') }} :</strong>
+            <input v-model="user.Birthdate" type="date" class="ui input styled-input" />
+          </p>
           <p><strong>{{ t('subscriptionStatus') }} :</strong>
             <span v-if="user.Current_Subscription" class="status-active">{{ t('subscriber') }}</span>
             <span v-else class="status-inactive">{{ t('nonSubscriber') }}</span>
           </p>
           <h3>{{ t('address') }}</h3>
-          <p><strong>{{ t('street') }} :</strong> {{ user.Street || t('noInfo') }}</p>
-          <p><strong>{{ t('city') }} :</strong> {{ user.City || t('noInfo') }}</p>
-          <p><strong>{{ t('state') }} :</strong> {{ user.State || t('noInfo') }}</p>
-          <p><strong>{{ t('postalCode') }} :</strong> {{ user.Postal_Code || t('noInfo') }}</p>
-          <p><strong>{{ t('country') }}:</strong> {{ user.Country || t('noInfo') }}</p>
+          <p><strong>{{ t('street') }} :</strong>
+            <input v-model="user.Street" type="text" class="ui input styled-input" />
+          </p>
+          <p><strong>{{ t('city') }} :</strong>
+            <input v-model="user.City" type="text" class="ui input styled-input" />
+          </p>
+          <p><strong>{{ t('state') }} :</strong>
+            <input v-model="user.State" type="text" class="ui input styled-input" />
+          </p>
+          <p><strong>{{ t('postalCode') }} :</strong>
+            <input v-model="user.Postal_Code" type="text" class="ui input styled-input" />
+          </p>
+          <p><strong>{{ t('country') }}:</strong>
+            <input v-model="user.Country" type="text" class="ui input styled-input" />
+          </p>
+
+          <!-- Champs mot de passe -->
+          <h3>{{ t('changePassword') }}</h3>
+          <p><strong>{{ t('currentPassword') }} :</strong>
+            <span>{{ showCurrentPassword ? user.Password : '********' }}</span>
+            <i :class="showCurrentPassword ? 'eye slash icon' : 'eye icon'"
+               @click="showCurrentPassword = !showCurrentPassword"></i>
+          </p>
+          <div class="password-section">
+            <p>
+              <strong>{{ t('newPassword') }} :</strong>
+              <input v-model="newPassword" :type="showNewPassword ? 'text' : 'password'" class="ui input styled-input password-input" />
+              <i :class="showNewPassword ? 'eye slash icon' : 'eye icon'"
+                 @click="showNewPassword = !showNewPassword"></i>
+            </p>
+            <p>
+              <strong>{{ t('confirmPassword') }} :</strong>
+              <input v-model="confirmPassword" :type="showConfirmPassword ? 'text' : 'password'" class="ui input styled-input password-input" />
+              <i :class="showConfirmPassword ? 'eye slash icon' : 'eye icon'"
+                 @click="showConfirmPassword = !showConfirmPassword"></i>
+            </p>
+          </div>
+
+          <button @click="updateUserDetails" class="ui teal button">{{ t('saveChanges') }}</button>
         </div>
 
         <router-view />
@@ -107,14 +231,13 @@ onMounted(() => {
 
 <style scoped>
 .spacer_perso {
-  margin: 7% ;
+  margin: 7%;
 }
 
-/* Adjustments to ensure the content area does not overlap with the menu */
 .content-area {
   padding: 20px;
-  margin-left: 50px; /* Match this with the actual width of the menu */
-  width: calc(70% - 50px); /* Ensure this calculation takes into account the menu's width */
+  margin-left: 50px;
+  width: calc(70% - 50px);
 }
 
 .header-section {
@@ -141,6 +264,20 @@ onMounted(() => {
   color: #333;
 }
 
+.styled-input {
+  border: 1px solid #ddd;
+  padding: 8px 10px;
+  border-radius: 4px;
+  width: calc(100% - 160px);
+  margin-left: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.styled-input:focus {
+  border-color: #00b5ad;
+  outline: none;
+}
+
 .status-active {
   color: green;
   font-weight: bold;
@@ -151,20 +288,30 @@ onMounted(() => {
   font-weight: bold;
 }
 
-h2 {
-  font-size: 2em;
-  margin-bottom: 20px;
-  color: #4a4a4a;
+.password-section p {
+  display: flex;
+  align-items: center;
 }
 
-h3 {
-  margin-top: 20px;
-  font-size: 1.5em;
-  color: #333;
+.password-input {
+  width: auto;
+  flex-grow: 1;
+  margin-left: 10px;
+}
+
+.eye.icon,
+.eye.slash.icon {
+  cursor: pointer;
+  margin-left: 10px;
 }
 
 .ui.red.button {
   background-color: #db2828;
+  color: white;
+}
+
+.ui.teal.button {
+  background-color: #00b5ad;
   color: white;
 }
 </style>
