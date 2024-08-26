63f324c7e29c521b24e0752a4d311336075fa884
Saiti91 <vellastephane91@gmail.com>
2024-08-26 10:06:49 +0200

    fin de test

--- Front/src/vue/backOffice/Categorie/CategoriesAdmin.vue
+++ Front/src/vue/backOffice/Categorie/CategoriesAdmin.vue
@@ -5,7 +5,6 @@ import HeaderBackOffice from "@/components/HeaderBackOffice.vue";
 import { useRouter } from 'vue-router';
 import { useI18n } from 'vue-i18n';
 
-//Pas de test
 const { t } = useI18n();
 const categories = ref([]);
 const router = useRouter();
